ed81400c43c82e8695868c34a15741ab4ba242ff
verIdyia <38138563+verIdyia@users.noreply.github.com>
2023-10-27 10:24:20 +0000

    update

--- js/links.js
+++ js/links.js
@@ -1,51 +1,51 @@
-export const links = [ 
-	{ 
-		name: "Twitch", 
-		link: 
-"https://www.twitch.tv/hejin0_0", 
-		image: 
-"./assets/twitch.png", 
-	}, 
-	{ 
-		name: "트게더", 
-		link: 
-"https://tgd.kr/s/hejin0_0", 
-		image: 
-"./assets/tgd.svg", 
-	}, 
-	{ 
-		name: "Youtube", 
-		link: 
-"https://www.youtube.com/@hejin0_0", 
-		image: 
-"./assets/youtube.png", 
-	}, 
-	{ 
-		name: "게임하는 헤징쿤", 
-		link: 
-"https://www.youtube.com/@user-mr2mc4ij5s", 
-		image: 
-"./assets/youtube.png", 
-	}, 
-	{ 
-		name: "E-mail", 
-		link: 
-"mailto:booall12@naver.com", 
-		image: 
-"./assets/mail.png", 
-	}, 
-	{ 
-		name: "방송 제목 알림이", 
-		link: 
-"https://discord.com/oauth2/authorize?client_id=1115119779854958644&permissions=2048&scope=bot", 
-		image: 
-"./assets/discord.png", 
-	}, 
-	{ 
-		name: "Instagram", 
-		link: 
-"https://www.instagram.com/hejin_x_x/", 
-		image: 
-"./assets/instagram.png", 
-	}, 
+export const links = [
+  {
+    name: "Twitch",
+    link:
+      "https://www.twitch.tv/hejin0_0",
+    image:
+      "./assets/twitch.svg",
+  },
+  {
+    name: "트게더",
+    link:
+      "https://tgd.kr/s/hejin0_0",
+    image:
+      "./assets/tgd.svg",
+  },
+  {
+    name: "Youtube",
+    link:
+      "https://www.youtube.com/@hejin0_0",
+    image:
+      "./assets/youtube.svg",
+  },
+  {
+    name: "게임하는 헤징쿤",
+    link:
+      "https://www.youtube.com/@user-mr2mc4ij5s",
+    image:
+      "./assets/youtube.svg",
+  },
+  {
+    name: "E-mail",
+    link:
+      "mailto:booall12@naver.com",
+    image:
+      "./assets/mail.svg",
+  },
+  {
+    name: "방송 제목 알림이",
+    link:
+      "https://discord.com/oauth2/authorize?client_id=1115119779854958644&permissions=2048&scope=bot",
+    image:
+      "./assets/discord.svg",
+  },
+  {
+    name: "Instagram",
+    link:
+      "https://www.instagram.com/hejin_x_x/",
+    image:
+      "./assets/instagram.svg",
+  },
 ];
