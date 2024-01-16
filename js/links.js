f2e6468c21db2322e21d5adba6569f91abd5cb49
verIdyia <38138563+verIdyia@users.noreply.github.com>
2024-01-16 17:32:22 +0900

    썸네일 갤러리 삭제

--- js/links.js
+++ js/links.js
@@ -7,6 +7,13 @@ export const links = [
       "./assets/twitch.svg",
   },
   {
+    name: "치지직",
+    link:
+      "https://chzzk.naver.com/live/21a0de8b586c517181d76f183272de57",
+    image:
+      "./assets/czz.png",
+  },
+  {
     name: "트게더",
     link:
       "https://tgd.kr/s/hejin0_0",
@@ -35,13 +42,6 @@ export const links = [
       "./assets/instagram.svg",
   },
   {
-    name: "치지직",
-    link:
-      "https://chzzk.naver.com/live/21a0de8b586c517181d76f183272de57",
-    image:
-      "./assets/czz.png",
-  },
-  {
     name: "방송 제목 알림이",
     link:
       "https://discord.com/oauth2/authorize?client_id=1115119779854958644&permissions=2048&scope=bot",
@@ -54,14 +54,6 @@ export const links = [
       "thumbnail",
     image:
       "./assets/youtube.svg",
-    sameTab: true,
-  },
-  {
-    name: "썸네일 갤러리(Beta)",
-    link:
-      "thumbnail_gallery",
-    image:
-      "./assets/hammer.svg",
     sameTab: true,
   },
 
