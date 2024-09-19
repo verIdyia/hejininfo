dc4577e98c63c27c8d2fda77a81abf4b182c13ab
verIdyia <38138563+verIdyia@users.noreply.github.com>
2024-09-19 21:16:36 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,35 +1,21 @@
-// imports
 import { links } from "./links.js";
-//constants
 
 const linkContainer = document.getElementById("links");
-
-//functions
 
 function addLink(name, link, image, sameTab = false) {
   const targetAttribute = sameTab ? '' : ' target="_blank"';
   return `
-  <a href="${link}" class="link"${targetAttribute}>
-    <img src="${image}" alt="${name}"/>
-    <span>${name}</span>
-  </a>
+    <a href="${link}" class="link"${targetAttribute}>
+      <img src="${image}" alt="${name}" />
+      <span>${name}</span>
+    </a>
   `;
 }
 
-// logic
-
 let allLinks = "";
 
-links.forEach((ele) => {
-  let link = ele.link;
-  let name = ele.name;
-  let image = ele.image;
-  let sameTab = ele.sameTab || false;
-
+links.forEach(({ name, link, image, sameTab }) => {
   allLinks += addLink(name, link, image, sameTab);
 });
-
-linkContainer.innerHTML = allLinks;
-
 
 linkContainer.innerHTML = allLinks;
