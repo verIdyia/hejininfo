eb2e57513a64290c4950c3efd60fcdac6828abbd
verIdyia <38138563+verIdyia@users.noreply.github.com>
2024-09-19 21:16:57 +0900

    Update links.js

--- js/links.js
+++ js/links.js
@@ -1,67 +1,48 @@
 export const links = [
   {
     name: "치지직",
-    link:
-      "https://chzzk.naver.com/live/21a0de8b586c517181d76f183272de57",
-    image:
-      "./assets/czz.png",
+    link: "https://chzzk.naver.com/live/21a0de8b586c517181d76f183272de57",
+    image: "./assets/czz.png",
   },
   {
     name: "팬카페",
-    link:
-      "https://cafe.naver.com/hejin0w0",
-    image:
-      "./assets/네이버_카페_로고.png",
+    link: "https://cafe.naver.com/hejin0w0",
+    image: "./assets/네이버_카페_로고.png",
   },
   {
     name: "Youtube",
-    link:
-      "https://www.youtube.com/@hejin0_0",
-    image:
-      "./assets/youtube.svg",
+    link: "https://www.youtube.com/@hejin0_0",
+    image: "./assets/youtube.svg",
   },
   {
     name: "게임하는 헤징쿤",
-    link:
-      "https://www.youtube.com/@user-mr2mc4ij5s",
-    image:
-      "./assets/youtube.svg",
+    link: "https://www.youtube.com/@user-mr2mc4ij5s",
+    image: "./assets/youtube.svg",
   },
   {
     name: "Instagram",
-    link:
-      "https://www.instagram.com/hejin_x_x/",
-    image:
-      "./assets/instagram.svg",
+    link: "https://www.instagram.com/hejin_x_x/",
+    image: "./assets/instagram.svg",
   },
   {
     name: "방송 제목 알림이",
-    link:
-      "https://discord.com/oauth2/authorize?client_id=1115119779854958644&permissions=2048&scope=bot",
-    image:
-      "./assets/discord.svg",
+    link: "https://discord.com/oauth2/authorize?client_id=1115119779854958644&permissions=2048&scope=bot",
+    image: "./assets/discord.svg",
   },
   {
     name: "썸네일 다운로더",
-    link:
-      "thumbnail",
-    image:
-      "./assets/youtube.svg",
+    link: "thumbnail",
+    image: "./assets/youtube.svg",
     sameTab: true,
   },
   {
     name: "Twitch",
-    link:
-      "https://www.twitch.tv/hejin0_0",
-    image:
-      "./assets/twitch.svg",
+    link: "https://www.twitch.tv/hejin0_0",
+    image: "./assets/twitch.svg",
   },
   {
     name: "트게더",
-    link:
-      "https://tgd.kr/s/hejin0_0",
-    image:
-      "./assets/tgd.svg",
+    link: "https://tgd.kr/s/hejin0_0",
+    image: "./assets/tgd.svg",
   },
-
 ];
